7714b3a40d0ba04d1ecfd069c0de08d7f7161701
Samuel <oiranca@gmail.com>
2021-09-20 10:44:50 +0100

    finished The Western Suburbs Croquet Club

--- src/katas/categorize_new_member/categorizeNewMember.js
+++ src/katas/categorize_new_member/categorizeNewMember.js
@@ -1,17 +1,17 @@
 export const newMemberCategorize = (data) => {
-  let typeMemberList = [];
-  const typeMember = {
-    OPEN: "Open",
-    SENIOR: "Senior",
-  };
+    let typeMemberList = [];
+    const typeMember = {
+        OPEN: "Open",
+        SENIOR: "Senior",
+    };
 
-  data.forEach(member=>{
-    for (const [age,handicap] of member){
-      age >= 55 && handicap > 7
-          ? typeMemberList.push(typeMember.SENIOR)
-          : typeMemberList.push(typeMember.OPEN);
+
+    for (const [age, handicap] of data) {
+        age >= 55 && handicap > 7
+            ? typeMemberList.push(typeMember.SENIOR)
+            : typeMemberList.push(typeMember.OPEN);
     }
-  })
 
-  return typeMemberList;
+
+    return typeMemberList;
 };
